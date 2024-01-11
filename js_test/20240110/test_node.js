270d1dee36ebb3c22d1774da2ecdf021e4582ef0
tesla <fushi1122@163.com>
2024-01-11 22:57:37 +0800

    Update test_node.js

--- js_test/20240110/test_node.js
+++ js_test/20240110/test_node.js
@@ -11,25 +11,5 @@ fs.readdir('./', (error, files) => {
 
 
 
-console.log("mater branch");
-
-console.log('ok');
-
-cosole.log('test github');
-
-console.log('test branch not up to date with origin!');
-
-console.log('main');
-
-console.log('main');
-
-console.log("test branch test");
-
-console.log('main');
-
-console.log('main');
-
-
-
 
 
